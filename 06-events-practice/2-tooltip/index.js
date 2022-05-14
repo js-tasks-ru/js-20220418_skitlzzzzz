08e32242f48974c321a1647c628ac79752bfb9c3
Dora <yppix@yandex.ru>
2022-05-15 02:54:53 +0300

    js-hw-6-2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,67 @@
 class Tooltip {
-  initialize () {
+  element = document.createElement('div');
+  static tip = document.createElement('div');
 
+  constructor() {
+    this.tooltipAttr = '';
+
+    if (!Tooltip._instance) {
+      Tooltip._instance = this;
+    }
+    return Tooltip._instance;
+
+  }
+
+
+  initialize() {
+    document.addEventListener('pointerover', this.showTooltip.bind(this), false);
+    document.addEventListener('pointerout', this.hideTooltip.bind(this), false);
+    document.addEventListener('mousemove', this.mouseMoving, false);
+    document.addEventListener('mouseout', this.mouseHide, false);
+  }
+
+
+  showTooltip(event) {
+    if (event.target.dataset.tooltip === undefined) {
+      return;
+    }
+    this.tooltipAttr = event.target.dataset.tooltip;
+    this.render();
+    Tooltip.tip = this.element;
+  }
+
+  render() {
+    this.element .innerHTML = `<div class="tooltip">${this.tooltipAttr}</div>`;
+    this.element = this.element .firstChild;
+    document.body.append(this.element);
+    return this.element ;
+  }
+
+  mouseMoving(event) {
+    Tooltip.tip.style.transform = 'translateY(' + (event.clientY - 60) + 'px)';
+    Tooltip.tip.style.transform += 'translateX(' + (event.clientX + 10) + 'px)';
+  }
+
+  mouseHide() {
+    document.removeEventListener('mousemove', this.mouseMoving, false);
+  }
+
+  hideTooltip() {
+    Tooltip.tip.remove();
+    document.removeEventListener('pointerover', this.showTooltip, false);
+  }
+
+  remove() {
+    document.removeEventListener('pointerover', this.showTooltip, false);
+    document.removeEventListener('pointerout', this.hideTooltip, false);
+    document.removeEventListener('mousemove', this.mouseMoving, false);
+    document.removeEventListener('mouseout', this.mouseHide, false);
+  }
+
+  destroy() {
+    this.remove();
+    this.element.remove();
+    Tooltip.tip.remove();
   }
 }
 
